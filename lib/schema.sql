5548d13ce96d552c5c5bfe2a19a0d0e84c08f1ea
Kriti Rai <kriti.rai@gmail.com>
2018-04-22 23:02:18 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE series (id PRIMARY KEY INTEGER, title TEXT, author_id INTEGER subgenre_id INTEGER);
+CREATE TABLE series (id PRIMARY KEY INTEGER, title TEXT, author_id INTEGER, subgenre_id INTEGER);
 -- CREATE TABLE subgenre (id PRIMARY KEY INTEGER, name TEXT);
 -- CREATE TABLE authors(id PRIMARY KEY INTEGER, name TEXT);
 -- CREATE TABLE books (id PRIMARY KEY INTEGER, title TEXT, series_id INTEGER);
